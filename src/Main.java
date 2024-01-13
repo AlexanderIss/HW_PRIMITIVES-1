cf43250af9e26c7a86349f42623d92be02a0e620
Neto test <bosgoty@mail.ru>
2024-01-13 15:22:48 +0700

    First-commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int priсe = 1678;                   // цена билета
+        int oneBonus = 20;                  // стоимость одного бонуса
+        int amountBonus = priсe / oneBonus; // вычисляем количество бонусов
+        System.out.println("Полученный бонус: " + amountBonus + " y.e.");    // вывод бонусов
+    }
+}
